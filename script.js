fe37f8e6146a2508492e3140a0d1f67d3e22ce9c
Ryan Geary <rtgnj42@gmail.com>
2024-08-27 22:02:19 -0400

    move imgs to dir

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-collection=[ "20240728_0293.jpg", "20240728_0540.jpg", "20240719_0037.jpg", "20240728_0830.jpg", "20240728_0114.jpg", "20240707_0177.jpg", "20240728_0089.jpg", ] 
+collection=[ "20240728_0293.jpg", "20240728_0540.jpg", "20240719_0037.jpg", "20240728_0830.jpg", "20240728_0114.jpg", "20240707_0177.jpg", "20240728_0089.jpg", ]
 
 // load all photos into main page
 const photosDiv = document.getElementById("photos");
@@ -7,6 +7,6 @@ const col2 = document.getElementById("col2");
 cols = [col1, col2]
 for (i = 0; i < collection.length; i++) {
     photo = document.createElement("photo-component")
-    photo.setAttribute("src", collection[i])
+    photo.setAttribute("src", "./img/"+collection[i])
     cols[i % 2].appendChild(photo)
 }
