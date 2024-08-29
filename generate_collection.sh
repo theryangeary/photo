2e15a2fc6e753840382a15eb68748ff75eab83b2
Ryan Geary <rtgnj42@gmail.com>
2024-08-28 21:56:16 -0400

    add tags

--- generate_collection.sh
+++ generate_collection.sh
@@ -1,2 +1,23 @@
 #!/bin/bash
-sed -i.bak "s/^collection=.*/$(find . -name '*.jpg' | xargs basename | gawk 'BEGINFILE{print "collection=["}{print "\"" $1 "\","}ENDFILE{print "]"}' | tr '\n' ' ')/" script.js
+
+function path_to_js_object() {
+    path=$1
+    basename="$(basename $path)"
+    output="{\"name\": \"$basename\""
+    tags="$(exiftool -All $path | grep -E "^Subject" | grep -Eo ":.*$" | tr -d ':' | tr -d ' ' | tr , '\n')"
+    output="$output , \"tags\": ["
+    first=true
+    for tag in $tags; do
+        if [ $first = true ]; then
+            output="$output \"$tag\""
+            first=false
+        else
+            output="$output , \"$tag\""
+        fi
+    done
+    output="$output ]}"
+    echo "$output"
+}
+export -f path_to_js_object
+
+sed -i.bak "s/^collection=.*/$(find . -name '*.jpg' | xargs -n 1 -I {} bash -c 'path_to_js_object "$@"' _ {} | gawk 'BEGINFILE{print "collection=["}{print $0 ","}ENDFILE{print "]"}' | tr '\n' ' ')/" script.js
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-collection=[ "20240728_0293.jpg", "20240728_0540.jpg", "20240719_0037.jpg", "20240728_0830.jpg", "20240728_0114.jpg", "20240707_0177.jpg", "20240728_0089.jpg", ]
+collection=[ {"name": "20240728_0293.jpg" , "tags": [ "pnw" ]}, {"name": "20240728_0540.jpg" , "tags": [ "critters" , "pnw" ]}, {"name": "20240719_0037.jpg" , "tags": [ "nyc" ]}, {"name": "20240728_0830.jpg" , "tags": [ "pnw" ]}, {"name": "20240728_0114.jpg" , "tags": [ "anna" , "pnw" ]}, {"name": "20240707_0177.jpg" , "tags": [ "longbranch" , "nj" ]}, {"name": "20240728_0089.jpg" , "tags": [ "pnw" ]}, ]
 
 // load all photos into main page
 const photosDiv = document.getElementById("photos");
@@ -7,6 +7,6 @@ const col2 = document.getElementById("col2");
 cols = [col1, col2]
 for (i = 0; i < collection.length; i++) {
     photo = document.createElement("photo-component")
-    photo.setAttribute("src", "./img/"+collection[i])
+    photo.setAttribute("src", "./img/"+collection[i].name)
     cols[i % 2].appendChild(photo)
 }
