89c83bc11ef0a2a118e563e266c06a2067973956
Ryan Geary <rtgnj42@gmail.com>
2024-09-11 17:26:58 -0400

    add x-out and escape to leave zoom in

--- components/fullover.js
+++ components/fullover.js
@@ -10,9 +10,33 @@ class Fullover extends HTMLElement {
     }
 
     connectedCallback() {
+        document.addEventListener('keydown', (event) => {
+            if (event.keyCode === 27) {
+                // Escape key pressed
+                this.hide()
+            }
+        });
+        this.render()
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        this.render()
+    }
+
+    hide() {
+        document.getElementById("fullover").classList.remove("show")
+    }
+
+    show() {
+        document.getElementById("fullover").classList.add("show")
+    }
+
+    render() {
+        const src = this.getAttribute('src')
         this.innerHTML = `
-<div id="fullover" onclick="remove(this)">
-    <img src="${this.getAttribute("src")}"/>
+<div id="fullover" onclick="document.getElementById('fullover-component').hide()">
+    <div onclick="document.getElementById('fullover-component').hide()" class="x-out"><a>&#x2715;</a></div>
+    <img src="${src}"/>
 </div>
 `
     }
