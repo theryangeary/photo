19be4a32c65b7edbdf8fcfb4e4059280b46c37e0
Ryan Geary <rtgnj42@gmail.com>
2024-12-04 18:15:06 -0500

    make navbar mobile friendly

--- components/navbar.js
+++ components/navbar.js
@@ -5,10 +5,10 @@ class Navbar extends HTMLElement {
 
     connectedCallback() {
         this.innerHTML = `
-<ul>
+<ul class="navbar" id="navbar">
     <li><a href="?">Home</a></li>
     <li class="dropdown">
-        <a href="javascript:void(0)" class="dropbtn">Selected Works</a>
+        <a href="javascript:void(0)" onclick="toggleDropdown(this)" class="dropbtn">Selected Works</a>
         <div class="dropdown-content">
             <a href="?tag=panorama">Panoramas</a>
             <a href="?selectedworks=street">Street Photography</a>
@@ -18,7 +18,7 @@ class Navbar extends HTMLElement {
         </div>
     </li>
     <li class="dropdown">
-        <a href="javascript:void(0)" class="dropbtn">On Location</a>
+        <a href="javascript:void(0)" onclick="toggleDropdown(this)" class="dropbtn">On Location</a>
         <div class="dropdown-content">
             <a href="?trip=pnw2024">Pacific Northwest</a>
             <a href="?trip=ecuador">Ecuador</a>
@@ -26,7 +26,7 @@ class Navbar extends HTMLElement {
         </div>
     </li>
     <li class="dropdown">
-        <a href="javascript:void(0)" class="dropbtn">Archive</a>
+        <a href="javascript:void(0)" onclick="toggleDropdown(this)" class="dropbtn">Archive</a>
         <div class="dropdown-content">
             <a href="?archive=202405">May 2024</a>
             <a href="?archive=202406">June 2024</a>
@@ -37,9 +37,46 @@ class Navbar extends HTMLElement {
             <a href="?archive=202411">November 2024</a>
         </div>
     </li>
+    <li class="icon">
+        <a href="javascript:void(0);" onclick="toggleNavbarResponsive()">
+            <i class="fa fa-bars"></i>
+        </a>
+    </li>
 </ul>
 `
     }
 }
+
+function toggleDropdown(t) {
+    if (window.matchMedia("(min-width: 600px)").matches) {
+        // don't set active on desktop, prefering hover
+        return
+    }
+    dropdown = t.parentElement
+    dropdowns = document.getElementsByClassName("dropdown")
+    if (dropdown.className === "dropdown") {
+        // ensure only one dropdown is active
+        for (i = 0; i < dropdowns.length; i++) {
+            if (dropdowns[i] === dropdown) {
+                continue
+            }
+            dropdowns[i].className = "dropdown"
+        }
+        dropdown.classList.add("active")
+    } else {
+        dropdown.className = "dropdown"
+    }
+}
+
+/* Toggle between adding and removing the "responsive" class to navbar when the user clicks on the icon */
+function toggleNavbarResponsive() {
+  var x = document.getElementById("navbar");
+  if (x.className === "navbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navbar";
+  }
+}
+
 
 customElements.define('navbar-component', Navbar);
