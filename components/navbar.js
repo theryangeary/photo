67c65446e52662dd28167f1e227f1c3cc90d4227
Ryan Geary <rtgnj42@gmail.com>
2025-01-03 15:32:28 -0500

    add 202412 navbar

--- components/navbar.js
+++ components/navbar.js
@@ -39,6 +39,7 @@ class Navbar extends HTMLElement {
             <a href="/photo/archive/202409">September 2024</a>
             <a href="/photo/archive/202410">October 2024</a>
             <a href="/photo/archive/202411">November 2024</a>
+            <a href="/photo/archive/202412">December 2024</a>
         </div>
     </li>
 </ul>
